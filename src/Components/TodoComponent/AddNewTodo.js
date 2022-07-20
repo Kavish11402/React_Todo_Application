9d504c608544f98df201ad5b5862794c0130bf26
Kavish <kavishmathur11402@gmail.com>
2022-07-20 18:07:35 +0530

    Add New Todo UI Updated and dark mode removed from it

--- src/Components/TodoComponent/AddNewTodo.js
+++ src/Components/TodoComponent/AddNewTodo.js
@@ -5,39 +5,114 @@ export default function AddNewTodo({isOpen, setIsOpen, addTodoTheme})
 
 
   return (
-      <Dialog
-          open={isOpen}
-          onClose={() => setIsOpen(false)}
-          className="relative z-50"
-      >
-          {/* The backdrop, rendered as a fixed sibling to the panel container */}
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+
           <div className="fixed inset-0 bg-black/60 backdrop-blur-lg"/>
 
-          {/* Full-screen container to center the panel */}
           <div className="fixed inset-0 flex items-center justify-center p-8">
-              {/* The actual dialog panel  */}
-              <Dialog.Panel className={addTodoTheme}>
-                      <Dialog.Title className="font-bold text-xl mt-4 ml-8">Create New Todo</Dialog.Title>
-                      <form className='px-10 py-6 flex flex-col'>
-
-                          <input type="text" placeholder='Add Todo Title' className='border border-2 mt-8 rounded-lg'/>
-                          <textarea placeholder='Add Todo Description' className='border border-2 mt-4 rounded-lg' rows="6" cols="4"/>
-                          <select className='mt-4 border rounded-lg' >
-
-                              <option>Work</option>
-                              <option>Personal</option>
-                              <option>Other</option>
-
-                          </select>
-                          <button className='h-16 w-60 bg-green-300 rounded-lg shadow mt-8'>Add New Todo</button>
 
 
-                      </form>
+              <div className={"w-1/2 rounded-xl bg-white"}>
 
 
+                  <form>
 
-              </Dialog.Panel>
+                      {/*Header*/}
+                      <header className={"bg-gray-400/50 rounded-t-xl text-center text-3xl font-bold py-2.5"} >Create New Todo</header>
+
+
+                      {/*Body*/}
+                      <div className={"py-5 px-8"}>
+
+                          <div className={"my-5"}>
+                              <p className={"mb-2 text-md font-bold"}>Title</p>
+                              <input type={"text"} required className="transition
+                                                                ease-in-out
+                                                                delay-100
+                                                                hover:-translate-y-1
+                                                                hover:scale-105
+                                                                duration-300
+                                                                w-full
+                                                                rounded-xl
+                                                                px-2
+                                                                border-2
+                                                                border-gray-400
+                                                                focus:border-indigo-500
+                                                                focus:shadow-md" />
+
+                          </div>
+
+                          <div className={"my-5"}>
+                              <p className={"mb-2 text-md font-bold"}>Category</p>
+                              <input type={"text"} required className="transition
+                                                                ease-in-out
+                                                                delay-100
+                                                                hover:-translate-y-1
+                                                                hover:scale-105
+                                                                duration-300
+                                                                w-full
+                                                                rounded-xl
+                                                                px-2
+                                                                border-2
+                                                                border-gray-400
+                                                                focus:border-indigo-500
+                                                                focus:shadow-md" />
+                          </div>
+
+                          <div className={"my-5"}>
+                              <p className={"mb-2 text-md font-bold"}>Description</p>
+                              <input type={"text"} required className="transition
+                                                                ease-in-out
+                                                                delay-100
+                                                                hover:-translate-y-1
+                                                                hover:scale-105
+                                                                duration-300
+                                                                w-full
+                                                                rounded-xl
+                                                                px-2
+                                                                border-2
+                                                                border-gray-400
+                                                                focus:border-indigo-500
+                                                                focus:shadow-md" />
+                          </div>
+
+                          <div className={"my-5"}>
+                              <p className={"mb-2 text-md font-bold"}>Date</p>
+                              <input type={"date"} required className="transition
+                                                                ease-in-out
+                                                                delay-100
+                                                                hover:-translate-y-1
+                                                                hover:scale-105
+                                                                duration-300
+                                                                w-full
+                                                                rounded-xl
+                                                                px-2
+                                                                border-2
+                                                                border-gray-400
+                                                                focus:border-indigo-500
+                                                                focus:shadow-md" />
+                          </div>
+
+                      </div>
+
+
+                      {/*Footer*/}
+                      <footer className={"bg-gray-400/50 rounded-b-xl py-2.5 flex justify-between"} >
+
+                          <button className={"bg-indigo-600 hover:bg-indigo-700 text-white shadow-sm text-sm font-medium mx-6 rounded-lg py-2 px-4"} onClick={()=>{setIsOpen(false)}}>Cancel</button>
+
+                          <button className={"bg-indigo-600 hover:bg-indigo-700 text-white shadow-sm text-sm font-medium mx-6 rounded-lg py-2 px-4"} >
+                              Add Todo
+                          </button>
+
+                      </footer>
+
+                  </form>
+
+
+              </div>
           </div>
+
       </Dialog>
   )
 }
